6395ce26ad21aacb31b5833a5e1629b68a402082
marekbecica <marekbecica@gmail.com>
2012-04-28 17:54:54 +0200

    shuffle pieces

--- jigsaw-puzzle/dart/Jigsaw.dart
+++ jigsaw-puzzle/dart/Jigsaw.dart
@@ -18,10 +18,31 @@ class Jigsaw{
     }
   }
   void randomizePlaces(){
-    
-    
+    List<int> listSorted = new List<int>();
+    for (int i = 0; i < amount; ++i)
+    {
+      listSorted.add(i);
+    }
+    shuffle(listSorted);
+    //listSorted.forEach((element) => print(element));
+    for (int i = 0; i < pieces.length; i++) {
+      pieces[i].setActPlace(listSorted[i]);
+      print("i=${i}:${listSorted[i]}");
+    }
   }
-  void insertDivs(int i, ImageElement img){
+  void shuffle(List<int> list) {
+    int len = list.length - 1;
+    for (int i = 0; i < len; i++) {
+      int index = (Math.random() * (len - i)).toInt() + i;
+      int tmp = list[i];
+      list[i] = list[index];
+      list[index] = tmp;
+    }
+  }
+
+  void insertDivs(Piece el, ImageElement img){
+    int i=el.getOrigPlace();
+    int a=el.getActPlace();
     StringBuffer sb=new StringBuffer();
     sb.add(document.query("#board").innerHTML);
     var sqrt=Math.sqrt(amount);
@@ -31,13 +52,18 @@ class Jigsaw{
     document.query("#board").innerHTML = sb.toString();
     Element elm=document.query("#piece_${i}");
     elm.style.position="absolute";
+    var leftPosition=a*((img.width)/sqrt).toInt();
+    if (a>(sqrt-1)){
+      leftPosition=a%sqrt*((img.width)/sqrt).toInt();
+    }
+    var topPosition=(a~/sqrt)*((img.height)/sqrt).toInt();
     var left=i*((img.width)/sqrt).toInt();
     if (i>(sqrt-1)){
       left=i%sqrt*((img.width)/sqrt).toInt();
     }
-    elm.style.left="${left}px";
+    elm.style.left="${leftPosition}px";
     var top=(i~/sqrt)*((img.height)/sqrt).toInt();
-    elm.style.top="${top}px";
+    elm.style.top="${topPosition}px";
     elm.style.width="${((img.width)/sqrt).toInt()}px";
     elm.style.height="${(((img.height)/sqrt)).toInt()}px";
     elm.style.background="url(\'img/img.jpg\') ${-left}px ${-top}px";
@@ -48,15 +74,9 @@ class Jigsaw{
     print(img.height);
     pieces=new List<Piece>();
     createPieces(amount);
-    pieces.forEach((element) => insertDivs(element.getOrigPlace(),img));
+    randomizePlaces();
+    pieces.forEach((element) => insertDivs(element,img));
     
-   
-    //document.query("#board").innerHTML = "<div id='piece_1' style='position:absolute; width: 50px; height:50px; background: url(\"img/img.jpg\") 300px 550px;''></div>";
-    
-    
-   
-    // HTMLElement sourceImage= (HTMLElement)(document.query("#sourceImage"));
-   //int srcImgHeight= ((HTMLImageElement)sourceImage).height();
     
   }
 }
